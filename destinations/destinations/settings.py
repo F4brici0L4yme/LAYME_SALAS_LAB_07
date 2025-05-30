5c01aafaf068b4d0b3281cf83d25a4ba0fbd017c
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-05-29 23:18:00 -0500

    Models updated DestinosTuristicos created and linked with views

--- destinations/destinations/settings.py
+++ destinations/destinations/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'travelApp',
 ]
 
 MIDDLEWARE = [
@@ -81,7 +82,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -110,20 +110,16 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- destinations/travelApp/views.py
+++ destinations/travelApp/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
-from .models import Destination
+from .models import DestinosTuristicos
 # Create your views here.
 
 
 def index(request):
 
-    dest1 = Destination()
+    dest1 = DestinosTuristicos()
     dest1.name = 'Mumbai'
     dest1.desc = 'The City That Never Sleeps'
     dest1.img = 'destination_1.jpg'
     dest1.price = 700
 
-    dest2 = Destination()
+    dest2 = DestinosTuristicos()
     dest2.name = 'Hyderabad'
     dest2.desc = 'First Biryani, Then Sherwani'
     dest2.img = 'destination_2.jpg'
     dest2.price = 650
 
-    dest3 = Destination()
+    dest3 = DestinosTuristicos()
     dest3.name = 'Bengaluru'
     dest3.desc = 'Beautiful City'
     dest3.img = 'destination_3.jpg'
